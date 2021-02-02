5cfc40b319a58ba2fa7e41b55c194cb4a5394884
MoHaMeD <mohamed@mohamed-PC>
2021-02-02 22:13:49 +0200

    app component

--- src/app/Components/track-order/track-order.component.ts
+++ src/app/Components/track-order/track-order.component.ts
@@ -13,17 +13,31 @@ export class TrackOrderComponent implements OnInit {
   ordersList: Order[] | any = [];
 
   constructor(private db: AngularFirestore) {
+
+    
     const orders = this.db.collection('Orders').valueChanges();
     orders.subscribe(
       (response) => {
         this.ordersList = response;
-        console.log(this.ordersList[1])
+        //console.log(this.ordersList[1])
         // console.log("success");
       },
       (error) => {
         console.log(error);
       }
     );
+
+
+    // db.collection('Orders').doc("WRQlvFUcx3PIgL8hZnfh").ref.get()
+    // .then(function (doc) {
+    //   if (doc.exists) {
+    //     console.log(doc.data());
+    //   } else {
+    //     console.log("There is no document!");
+    //   }
+    // }).catch(function (error) {
+    //   console.log("There was an error getting your document:", error);
+    // });
 
   }
 
