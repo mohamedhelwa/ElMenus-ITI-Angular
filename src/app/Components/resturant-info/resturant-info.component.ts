a5d1b1c2cbfcd5eff0e80c9e0e62213468df3eec
lobna Ibrahem <30407102+lobnaibrahem@users.noreply.github.com>
2021-02-03 18:21:16 +0200

    final--1

--- src/app/Components/resturant-info/resturant-info.component.ts
+++ src/app/Components/resturant-info/resturant-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Restaurants } from 'src/app/ViewModels/restaurants';
 
 @Component({
   selector: 'app-resturant-info',
@@ -7,7 +8,10 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['./resturant-info.component.scss','../resturant/resturant.component.scss']
 })
 export class ResturantInfoComponent implements OnInit {
+  
   list:any[];
+  listofres:Restaurants | any ;
+
   constructor(private db:AngularFirestore) { }
 
   ngOnInit(): void {
@@ -15,6 +19,16 @@ export class ResturantInfoComponent implements OnInit {
     d.subscribe(
       (response) => {
         this.list = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    const r = this.db.collection('Restaurants').valueChanges();
+    r.subscribe(
+      (response) => {
+        this.listofres = response[0];
+        console.log(this.listofres)
       },
       (error) => {
         console.log(error);
--- src/app/Components/resturant-menu/resturant-menu.component.ts
+++ src/app/Components/resturant-menu/resturant-menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Dishes } from 'src/app/ViewModels/dishes';
 
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-resturant-menu',
   templateUrl: './resturant-menu.component.html',
@@ -9,16 +11,18 @@ import { Dishes } from 'src/app/ViewModels/dishes';
 })
 export class ResturantMenuComponent implements OnInit {
 
-  menu:Dishes[] | any;
-  
-  constructor(private db:AngularFirestore) { }
+    closeResult='';
+    menu:Dishes[] | any;
+    DishBuy:Dishes[];
+    add:number = 1;
+  constructor(private db:AngularFirestore , private modalService: NgbModal) { }
 
   ngOnInit(): void {
     const d = this.db.collection('Dishes').valueChanges();
     d.subscribe(
       (response) => {
         this.menu = response;
-        console.log(this.menu)
+        // console.log(this.menu)
       },
       (error) => {
         console.log(error);
@@ -26,4 +30,38 @@ export class ResturantMenuComponent implements OnInit {
     );
 
   }
+  buy(i){
+    this.DishBuy= i;
+    console.log(i)
+    console.log(this.DishBuy)
+  }
+  addOrder(){
+    this.add+=1;
+  }
+  remOrder(){
+    if(this.add <=1){
+      this.add = 0;
+    }else{
+      this.add-=1;
+    }
+    
+  }
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
 }
